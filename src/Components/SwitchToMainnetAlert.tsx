53bc8f31c061f29038759cb73cd34614a0a67834
Alex Priadko <mr.eskrano@gmail.com>
2021-09-05 20:20:36 +0300

    Fix

--- src/Components/SwitchToMainnetAlert.tsx
+++ src/Components/SwitchToMainnetAlert.tsx
@@ -1,20 +1,14 @@
 import React, { FC } from 'react'
-import { Container, Button, Alert } from 'react-bootstrap'
-import { useEthers } from '@usedapp/core'
+import { Container, Alert } from 'react-bootstrap'
 
 interface SwitchToMainnetAlertProps { }
 
 export const SwitchToMainnetAlert: FC<SwitchToMainnetAlertProps> = (props) => {
-
-    const { deactivate } = useEthers()
-
     return (
         <Container className = "mt-3 m-auto">
             <Alert variant="danger">
                 Please switch to Ethereum mainnet.
             </Alert>
-            <hr />
-            <Button onClick={() => deactivate()} variant="danger">Logout</Button>
         </Container>
     )
 }
